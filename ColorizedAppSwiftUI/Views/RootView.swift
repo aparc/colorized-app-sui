74d5b4032ca83594d22ced07cacddc341252c7f9
Andrei <a.parchukov@yandex.ru>
2022-09-13 23:12:43 +0300

    renamed

--- ColorizedAppSwiftUI/Views/RootView.swift
+++ ColorizedAppSwiftUI/Views/RootView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
     
     @FocusState private var focusedColorField: Color?
     
@@ -79,6 +79,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RootView()
     }
 }
